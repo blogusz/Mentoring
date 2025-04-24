9ab70a3c0d9106585d5f0dc34638dc76b4ec4d6c
Bogusz Laszczyk <blogusztrash@gmail.com>
2025-04-24 19:27:26 +0200

    SCRUM-4: Update main.py script to use sports_api package

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-from data_scraper import scrape_all_rounds
-from tools import load_config
+from pprint import pprint
+
+from sports_api.api_client import ApiClient
 
 if __name__ == '__main__':
-    api_key, base_url = load_config()
+    api_client = ApiClient()
+
+    # Get all rounds for a league
+    rounds_data = api_client.get_all_rounds(
+        league_id=4335,  # Spanish La Liga
+        season='2024-2025',
+        start_round=1,
+        end_round=5,
+        output_file='data.json',
+        save_data=True
+    )
+    pprint(rounds_data)
+
+    # Example of using other API methods
+    # leagues = api_client.get_all_leagues()
+    # pprint(leagues)
+    # team = api_client.search_team("Barcelona")
+    # pprint(team)
